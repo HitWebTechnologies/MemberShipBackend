d13c4703f797a4dc819fc21f6b5e0d8a763e40f8
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-03 23:48:52 +0200

    bootstrap together all the modules to form  the app
    -- this is the entry point of the app and only
       starts the app if the database is upp and running

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+const routes = require('./members/routes')
+app.use(routes)
+// 404 - route not found
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'The API endpoint you reached does not exist. The only valid ones are POST /register and POST /register-login/:id'
+  })
+})
+
+const initializeDatabases = require('./dbs')
+
+initializeDatabases().then(db => {
+  // Initialize the application once database connections are ready.
+  app.listen(3000, () => console.log('Listening on port 3000'))
+}).catch(err => {
+  console.error('Failed to make all database connections!')
+  console.error(err)
+  process.exit(1)
+})
