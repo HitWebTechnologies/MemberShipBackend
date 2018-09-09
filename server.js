52daa262a23b063ff78d85262005a056089cb9d5
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-09 06:24:55 +0200

    Only allow requests made from app.hitwebtech.co.zw

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'https://app.hitwebtech.co.zw' }))
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
